55400b6c3867aa0dee2f3190ebff646cd953830b
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2018-06-29 15:20:12 +0300

    .

--- testCucumberWithJUnit/src/test/java/com/probe/MathsStepsTests.java
+++ testCucumberWithJUnit/src/test/java/com/probe/MathsStepsTests.java
@@ -5,7 +5,7 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
 
-public class MathsSteps {
+public class MathsStepsTests {
     int firstNumber;
     int secondNumber;
     int result;
