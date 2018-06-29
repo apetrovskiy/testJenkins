10383b6e1deada5606db23a8f94ff9c280887267
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2018-06-30 00:06:53 +0300

    .

---
+++ testCucumberWithJUnit/src/test/java/com/probe/MasterTest.java
@@ -0,0 +1,9 @@
+package com.probe;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(MathsStepsTests.class)
+public class MasterTest {
+}
